4952531d00d1961d0904d8278e72630cd1bdb3cf
Marco Platino <mistermarcop@hotmail.com>
2025-06-23 21:03:46 -0400

    Added the actual encoding algorithm of the CaeserCipher.py, which I am very proud of, because I worked through it from nothing, without any help from the internet. It works perfectly. There could be some problems with the encoding of special characters, but I will fix that later. Also, with longer strings, it may get innefficient, but I have not completely tested it yet.

---
+++ CaeserCipher.py
@@ -0,0 +1,18 @@
+def shift(text, shift):
+    listShift = list(text)
+    encodedList = []
+    for i in listShift:
+        base = ord(i)
+        over = base + shift
+        if over > 122:
+            dif = 122 - (base + shift)
+            print(dif)
+            encodedList.append(chr(96 + abs(dif)))
+            continue
+        encodedList.append(chr((base) + (shift)))
+    return "".join(encodedList)
+
+print(shift("hello", 13))
+
+
+        

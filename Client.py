071a711f332bd0d6dad77a736f033dbeda7e3201
Marco Platino <mistermarcop@hotmail.com>
2025-06-25 20:08:48 -0400

    fic

--- Client.py
+++ Client.py
@@ -4,8 +4,11 @@ port = 12346
 
 p = " "
 while p != "stop":
-        s = socket.socket()         
-        s.connect(('192.168.86.205', port)) 
+        s = socket.socket()
+        try:      
+            s.connect(('192.168.86.205', port)) 
+        except:
+              print("connection to server failed!?!")
         message = (s.recv(1024).decode())
         print(f'Recieved From Server: {message}')
         s.send(input("Reply: ").encode())

0c231417b2c5e5e4b1236326d12aa18077f9cc6d
Marco Platino <mistermarcop@hotmail.com>
2025-06-26 14:33:19 -0400

    added branch for testing!?!?

--- Server.py
+++ Server.py
@@ -1,4 +1,19 @@
 import socket
+from _thread import start_new_thread
+import threading
+
+lock = threading.Lock()
+
+
+def handle_client(c):
+    while True:
+        data = c.recv(1024)
+        if not data:
+            print('Bye')
+            lock.release()
+            break
+        c.send(data[::-1])
+    c.close()
 
 s = socket.socket()
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -14,12 +29,18 @@ print("socket is listening")
 
 message = ""
 while message != "stop":
+    # while True:
+    #     message = input("what to sendus: ")
+    #     c, addr = s.accept()
+    #     # print('Got connection from', addr)
+    #     c.send(message.encode())
+    #     print(f'Recieved From Client: {c.recv(1024).decode()}')
+    #     c.close()
+    #     break
     while True:
-        message = input("what to sendus: ")
         c, addr = s.accept()
-        # print('Got connection from', addr)
-        c.send(message.encode())
-        print(f'Recieved From Client: {c.recv(1024).decode()}')
-        c.close()
-        break
+        lock.acquire()
+        print('Connected to:', addr[0], ':', addr[1])
+        start_new_thread(handle_client, (c,))
+
 s.close()

9e81f65374c2433b2f09ce0254f3fdaa24e75d80
Marco Platino <mistermarcop@hotmail.com>
2025-06-26 14:43:36 -0400

    Basically rewrote the whole client-side to allow for multi-threading.

--- Client.py
+++ Client.py
@@ -1,16 +1,21 @@
 import socket             
 
-port = 12346       
+port = 12346 
+host ='192.168.86.205'   
 i = 0
 p = " "
-while p != "stop":
-        s = socket.socket()
+s = 0
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    while True:
         try:
             s.connect(('192.168.86.205', port))
             print("--------------------------------------------------")
             print("Connected to server successfully!")
             print("--------------------------------------------------")
             i = 0
+            break
         except Exception as e:
             i += 1        
             print(f"Connection to server failed!?!  Attempt {i}. Retrying...")
@@ -18,9 +23,23 @@ while p != "stop":
                  print("Failed to connect after 10 attempts. Exiting.")
                  break
             continue
-        message = (s.recv(1024).decode())
-        s.send('[GOT THE MESSAGE SUCESSFULLY]'.encode())
-        print(f'Recieved From Server: {message}')
-        s.send(input("Reply: ").encode())
-        s.close()
-print("The thing is done.")
+
+while True:
+    main()
+    msg = input("What to send to the server: ")
+    if msg == 'ni':
+        break
+    s.send(msg.encode())
+    data = s.recv(1024)
+    print(f'Recieved: {data.decode()}')
+s.close()
+
+# while p != "stop":
+#         s = socket.socket()
+        
+#         message = (s.recv(1024).decode())
+#         s.send('[GOT THE MESSAGE SUCESSFULLY]'.encode())
+#         print(f'Recieved From Server: {message}')
+#         s.send(input("Reply: ").encode())
+#         s.close()
+# print("The thing is done.")
--- Demo Stuff/client2demo.py
+++ Demo Stuff/client2demo.py
@@ -1,8 +1,8 @@
 import socket
 
 def main():
-    host = '127.0.0.1'
-    port = 12345
+    host = '192.168.86.205'
+    port = 12346
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))

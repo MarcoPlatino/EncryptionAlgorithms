e7963906c4a60871adf243279f5bf348d67260e8
Marco Platino <mistermarcop@hotmail.com>
2025-06-27 09:47:13 -0400

    fixed server logic

--- Server.py
+++ Server.py
@@ -2,18 +2,31 @@ import socket
 from _thread import start_new_thread
 import threading
 
+lock = threading.Lock()
 
-def handle_client(c, i):
+clients = {}
+
+
+def handle_client(c, i, addr):
     client_id = c.recv(1024).decode().strip()
     print(f'Client ID: {client_id}')
-    while True:
-        data = c.recv(1024)
-        if not data:
-            print(f'disconnected from client {client_id}')
-            break
-        c.send(data[::-1])
-        i += 1
-        print(f'request: {i} on client: {client_id}')
+    lock.acquire()
+    clients[client_id] = c
+    print(clients)
+    lock.release()
+    try:
+        while True:
+            message = c.recv(1024)
+            message = message.decode()
+            recipient = message[:1]
+            clients[recipient].send(message.encode())
+    finally:
+        lock.acquire()
+        if client_id in clients:
+            del clients[client_id]
+        lock.release()
+        c.close()
+
     c.close()
 
 s = socket.socket()
@@ -29,6 +42,7 @@ print("socket is listening")
 print("----------------------------------------------")
 i = 0
 message = ""
+b = 0
 while message != "stop":
     # while True:
     #     message = input("what to sendus: ")
@@ -41,6 +55,7 @@ while message != "stop":
     while True:
         c, addr = s.accept()
         print('Connected to:', addr[0], ':', addr[1])
-        start_new_thread(handle_client, (c,i))
+        b += 1
+        start_new_thread(handle_client, (c,i,addr))
 
 s.close()

b6d19ad5ec294b78eee1d80a2754246d7b0fdae7
Marco Platino <mistermarcop@hotmail.com>
2025-06-29 17:56:16 -0400

    Rewrote the client *AGAIN* because it broke the server.

--- RealClient.py
+++ RealClient.py
@@ -4,24 +4,30 @@ import queue
 
 output = queue.Queue()
 
-def sendMessage():
+def sendMessage(s):
     while True:
         msg = input("What to send to the server: ")
         if msg == 'ni':
             break
         s.send(msg.encode())
     s.close()
-port = 12346 
-host ='192.168.86.205'   
-i = 0
-p = " "
 
-send = threading.Thread(target=sendMessage, daemon=True)
-send.start()
-def main():
-    i = 0
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def recieveMessage(s):
     while True:
+        try:
+            data = s.recv(1024)
+            if not data:
+                break
+            print(f"Recieved: {data.decode()}")
+            s.send(b'yes')
+        except:
+            break
+
+def main():
+    port = 12346 
+    host ='192.168.86.205'  
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    for i in range(10):
         try:
             s.connect(('192.168.86.205', port))
             print("--------------------------------------------------")
@@ -34,23 +40,9 @@ def main():
         except Exception as e:
             i += 1        
             print(f"Connection to server failed!?!  Attempt {i}. Retrying...")
-            if i >= 10:
-                 print("Failed to connect after 10 attempts. Exiting.")
-                 break
-            continue
-    while True:
-        data = s.recv(1024)
-        print(f'Recieved: {data.decode()}')
+            if i == 10:
+                print("Failed to connect after 10 attempts. Exiting.")
+                return
+        threading.Thread(target=sendMessage, args=(s,), daemon=True).start()
+        recieveMessage()
 
-s = main()
-
-
-# while p != "stop":
-#         s = socket.socket()
-        
-#         message = (s.recv(1024).decode())
-#         s.send('[GOT THE MESSAGE SUCESSFULLY]'.encode())
-#         print(f'Recieved From Server: {message}')
-#         s.send(input("Reply: ").encode())
-#         s.close()
-# print("The thing is done.")
